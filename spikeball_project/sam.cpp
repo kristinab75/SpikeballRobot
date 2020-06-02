1279064a8c49d4d52833ac768e43e029d9489141
Samantha Beaulieu <sambeau@stanford.edu>
2020-06-02 15:41:25 -0700

    get which robot should act next

--- spikeball_project/sam.cpp
+++ spikeball_project/sam.cpp
@@ -30,35 +30,31 @@ Vector3d ballDetection::getNoisyPosition(Vector3d& pos) {
     return pos;
 }
 
-int ballDetection::getRobot(Vector3d& endPos) {
-    int botNum = 0;
-    Vector3d start << 0,0,0;
-    Vector3d robot1org << 0,1.3,0;
-    Vector3d robot2org << 1.3,0,0;
-    Vector3d robot3org << 0,-1.3,0;
-    Vector3d robot4org << -1.3,0,0;
-    double dist1 = 0;
-    double dist2 = 0;
-    double dist3 = 0;
-    double dist4 = 0;
-    if (endPos == start) {
-        return 0;
-    } else {
-        for (int i=0; i<3; i++) {
-            dist1 = dist1 + ((endPos[i]-robot1org[i])*(endPos[i]-robot1org[i]));
-            dist2 = dist2 + ((endPos[i]-robot2org[i])*(endPos[i]-robot2org[i]));
-            dist3 = dist3 + ((endPos[i]-robot3org[i])*(endPos[i]-robot3org[i]));
-            dist4 = dist4 + ((endPos[i]-robot4org[i])*(endPos[i]-robot4org[i]));
-        }
-        if (dist1 > dist2 && dist1 > dist3 && dist1 > dist4) {
-            botNum = 1;
-        } else if (dist2 > dist1 && dist2 > dist3 && dist2 > dist4) {
-            botNum = 2;
-        } else if (dist3 > dist1 && dist3 > dist2 && dist3 > dist4) {
-            botNum = 3;
+int ballDetection::getRobot(Vector3d& x_vel_ball, bool& sameTeam, int robotDes) {
+    /* must initialize robotDes = 0 outside this function
+     for the first move of initializing the ball sameTeam = false, goes into if statement
+     to assign a value for robotDes. Then it always keeps track of who has the ball. 
+     */
+    if (sameTeam == false) {
+        if (x_vel_ball[0] > 0 && x_vel_ball[1] > 0) {
+            robotDes = 1;
+        } else if (x_vel_ball[0] > 0 && x_vel_ball[1] <= 0) {
+            robotDes = 4;
+        } else if (x_vel_ball[0] <= 0 && x_vel_ball[1] > 0) {
+            robotDes = 2;
         } else {
-            botNum = 4;
+            robotDes = 3;
         }
-        return botNum;
+    } else {
+        if (robotDes == 1) {
+            robotDes = 4;
+        } else if (robotDes == 4) {
+            robotDes = 1;
+        } else if (robotDes == 2) {
+            robotDes == 3;
+        } else {
+            robotDes == 2;
+        }
     }
+    return robotDes;
 }
